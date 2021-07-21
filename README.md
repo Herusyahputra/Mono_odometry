85d54a135b13dd4a7e17e02122967490257bdaa5
aji-ptn <ajipamungkastrinurcahyo@gmail.com>
2021-07-22 01:58:44 +0800

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+# Mono Visual Odometry using Kitti dataset
 
 # Algorithm
 Steps of the algorithm are taken from Avi Singh's blog post mentioned above. 
@@ -12,14 +13,51 @@ For each of the steps above, the line of code is provided to the exact location 
 
 The dataset used is: [KITTI Visual Odometry](http://www.cvlibs.net/datasets/kitti/eval_odometry.php)
 
+Or you can use this link to download [this dataset link](https://mcut-my.sharepoint.com/:f:/g/personal/m09158022_o365_mcut_edu_tw/EvVz4l8UCB1Pp3vxAP9tlXIB0UxbmyCjVTn5ITFxAVOV0Q?e=iaCoiS)
+1. [image sequence](https://s3.eu-central-1.amazonaws.com/avg-kitti/data_odometry_gray.zip)
+2. [pose image](https://s3.eu-central-1.amazonaws.com/avg-kitti/data_odometry_poses.zip)
+# Requirement
+
+1. Python 3.**
+2. opencv-python
+3. numpy
 # Running Program
 1. First clone repository
-2. In `test.py` change `img_path` and `pose_path` to correct image sequences and pose file paths
+   
+```buildoutcfg
+$ git clone https://github.com/aji-ptn/Mono_odometry.git
+```
+2. In `main.py` change `img_path` and `pose_path` to correct image sequences and pose file paths in **line 7 and 8**
+   
+```buildoutcfg
+img_path = '/home/aji/Documents/Dataset/data_odometry_gray/dataset/sequences/00/image_0/'
+pose_path = '/home/aji/Documents/Dataset/data_odometry_poses/dataset/poses/00.txt'
+```
 3. Ensure focal length and principal point information is correct
-4. Adjust Lucas Kanade Parameters as needed
-5. Run command `python ./test.py`
+   
+```buildoutcfg
+focal = 718.8560  # focal length
+pp = (607.1928, 185.2157)  # principal point
+```
+4. Adjust Lucas Kanade Parameters as needed **(line 16 in main.py)**
+   
+```buildoutcfg
+lk_params = dict(winSize=(21, 21), criteria=(cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 30, 0.01))
+```
+5. Run command 
+   
+```buildoutcfg
+main.py
+````
+# Result
+
+![img.png](img.png)
+![img_1.png](img_1.png)
+
+## You can click [this video](https://www.youtube.com/watch?v=vASenN5PiPc)
+[![video](img_2.png)](https://www.youtube.com/watch?v=vASenN5PiPc)
 
 # Reference
 An invaluable resource I used in building the visual odometry system was Avi Singh's blog post: http://avisingh599.github.io/vision/monocular-vo/ as well as his C++ implementation found [here](https://github.com/avisingh599/mono-vo).
 
-Datasets that can be used: [http://www.cvlibs.net/datasets/kitti/eval_odometry.php](http://www.cvlibs.net/datasets/kitti/eval_odometry.php)
+# Aji Pamungkas Tri Nurcahyo
--- main.py
+++ main.py
@@ -1,33 +1,30 @@
 import numpy as np
 import cv2 as cv
 # import matplotlib.pyplot as plt
-from monovideoodometery import MonoVideoOdometery
+from imutils import Mono_Odometery
 import os
 
-
-img_path = '/home/aji/Documents/Dataset/data_odometry_gray/dataset/sequences/00/image_1/'
+img_path = '/home/aji/Documents/Dataset/data_odometry_gray/dataset/sequences/00/image_0/'
 pose_path = '/home/aji/Documents/Dataset/data_odometry_poses/dataset/poses/00.txt'
 
 focal = 718.8560  # focal length
 pp = (607.1928, 185.2157)  # principal point
 R_total = np.zeros((3, 3))
-t_total = np.empty(shape=(3, 1))
+t_total = np.zeros(shape=(3, 1))
 
 # Parameters for lucas kanade optical flow
-lk_params = dict( winSize  = (21,21),
-                  criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 30, 0.01))
+lk_params = dict(winSize=(21, 21), criteria=(cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 30, 0.01))
 
-output=open("translation.txt", "w")
 # Create some random colors
-color = np.random.randint(0,255,(5000,3))
+color = np.random.randint(0, 255, (5000, 3))
 
-vo = MonoVideoOdometery(img_path, pose_path, focal, pp, lk_params)
+vo = Mono_Odometery(img_path, pose_path, focal, pp, lk_params)
 traj = np.zeros(shape=(600, 800, 3))
 
 # mask = np.zeros_like(vo.current_frame)
 # flag = False
-while(vo.hasNextFrame()):
 
+while (vo.hasNextFrame()):
     frame = vo.current_frame
 
     # for i, (new,old) in enumerate(zip(vo.good_new, vo.good_old)):
@@ -38,7 +35,6 @@ while(vo.hasNextFrame()):
     #         if flag:
     #             mask = cv.line(mask, (a,b),(c,d), color[i].tolist(), 2)
     #             frame = cv.circle(frame,(a,b),5,color[i].tolist(),-1)
-
 
     # cv.add(frame, mask)
     cv.imshow('frame', frame)
@@ -70,16 +66,16 @@ while(vo.hasNextFrame()):
     traj = cv.circle(traj, (true_x + 400, true_z + 100), 1, list((255, 255, 255)), 4)
     traj = cv.circle(traj, (draw_x + 400, draw_z + 100), 1, list((0, 255, 0)), 4)
 
-    cv.putText(traj, 'Actual Position:', (110, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5,(255,255,255), 1)
-    cv.putText(traj, 'White', (250, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5,(255, 255, 255), 1)
-    cv.putText(traj, 'Estimated Odometry Position:', (0, 50), cv.FONT_HERSHEY_SIMPLEX, 0.5,(255,255,255), 1)
-    cv.putText(traj, 'Green', (250, 50), cv.FONT_HERSHEY_SIMPLEX, 0.5,(0, 255, 0), 1)
-    cv.putText(traj, 'Fast, Aji-ptn', (690, 590), cv.FONT_HERSHEY_SIMPLEX, 0.5,(0, 255, 255), 1)
+    cv.putText(traj, 'Actual Position:', (110, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+    cv.putText(traj, 'White', (250, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+    cv.putText(traj, 'Estimated Odometry Position:', (0, 50), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+    cv.putText(traj, 'Green', (250, 50), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+    cv.putText(traj, 'Fast, Aji-ptn', (690, 590), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1)
 
-    value = [mono_coord, true_coord, draw_x, draw_y, draw_z, true_x, true_y, true_z]
-    with open("translation.txt", "w") as output:
-        output.write(str(value))
-        output.write(str("\n"))
+    # value = [mono_coord, true_coord, draw_x, draw_y, draw_z, true_x, true_y, true_z]
+    # with open("translation.txt", "w") as output:
+    #     output.write(str(value))
+    #     output.write(str("\n"))
 
     cv.imshow('trajectory', traj)
 cv.imwrite("./images/trajectory.png", traj)
